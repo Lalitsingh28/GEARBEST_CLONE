33989446eb0e2b1f32a1d9024af0fd2565f878f6
unknown <lalit4283@gmail.comgit config --global user.name Lalitsingh28>
2022-06-19 16:57:48 +0530

    final

---
+++ signin.js
@@ -0,0 +1,42 @@
+
+let userdata=JSON.parse(localStorage.getItem("user"));
+console.log(userdata)
+let form = document.getElementById("form");
+form.addEventListener("submit",function(event){
+console.log("yes")
+
+    event.preventDefault();
+    let data = {
+        email:form.mail.value,
+        pass:form.passwd.value,
+        
+    }
+    console.log(data)
+    if(userdata == "")
+    {
+      
+        alert("Create your account first");
+    }
+  else if(userdata.passwd==data.pass && userdata.email==data.email)
+  {
+    localStorage.setItem("sign",JSON.stringify(userdata))
+    window.location.href="super.html"
+
+    alert("Signin Successfully");
+  }
+  else
+  {
+    alert("User doen't exist");
+  }
+});
+
+function mysign(checkbox)
+{
+  if(checkbox.checked==true)
+  {
+    document.getElementById("btn").removeAttribute("disabled")
+  }
+  else{
+    document.getElementById("btn").setAttribute("disabled","disabled")
+  }
+}
