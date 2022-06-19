e63023e9d4d63e21257f9758e3317b38f92d26e7
roni8420 <101327545+roni8420@users.noreply.github.com>
2022-06-19 10:17:33 +0530

    Update payment.js

--- payment.js
+++ payment.js
@@ -1,7 +1,34 @@
 
+var arr = JSON.parse(localStorage.getItem("user"));
+
 
 
 function click2(){
     alert("Order placed");
     window.location.href = "last.html"
+}
+
+
+document.getElementById("paypal").addEventListener("click",paypalFunction)
+
+
+function paypalFunction(){
+    var data = JSON.parse(localStorage.getItem("user"));
+    console.log(data)
+    display(data)
+}
+
+function display(data){
+    document.getElementById("appendHere").innerHTML = "";
+  
+        let box = document.createElement("div");
+
+        let add = document.createElement("h3");
+    
+        add.innerText= data.add1;
+        box.append(add);
+        document.getElementById("appendHere").append(box);
+   
+    
+    
 }
